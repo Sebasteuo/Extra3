df7e50357e7db4317f58960ac5e6d416eab629fc
Hack998 <38637984+Hack998@users.noreply.github.com>
2018-04-24 21:47:41 -0600

    TreeNode
    Separacion de TreeNode

--- src/arbol/ArbolBusquedaBinaria.java
+++ src/arbol/ArbolBusquedaBinaria.java
@@ -1,17 +1,7 @@
+package arbol;
+
 public class ArbolBusquedaBinaria {
-	public static class TreeNode {
-		int data;
-		TreeNode left;
-		TreeNode right;
- 
-		TreeNode(int data) {
-			this.data = data;
-		}
 
-
-	}
-	
-	// Método que obtiene el menor elemento
 	public static TreeNode elementoMenor(TreeNode root) {
 		if (root.left == null)
 			return root;
@@ -19,7 +9,7 @@ public class ArbolBusquedaBinaria {
 			return elementoMenor(root.left);
 		}
 	}
- 
+
 	public static TreeNode eliminar(TreeNode root, int value) {
 		if (root == null)
 			return null;
@@ -27,18 +17,18 @@ public class ArbolBusquedaBinaria {
 			root.left = eliminar(root.left, value);
 		} else if (root.data < value) {
 			root.right = eliminar(root.right, value);
- 
+
 		} else {
-			// Verifica Si el nodo a eliminar tiene ambos hijos 
+			// Verifica Si el nodo a eliminar tiene ambos hijos
 			if (root.left != null && root.right != null) {
 				TreeNode temp = root;
 				// Busca el elemento menor de la derecha
 				TreeNode minNodeForRight = elementoMenor(temp.right);
-				// Remplaza el nodo current con el nodo menor del subárbol derecho
+				// Remplaza el nodo current con el nodo menor del sub-arbol derecho
 				root.data = minNodeForRight.data;
-				// Borra el nodo menor del subárbol derecho 
+				// Borra el nodo menor del subárbol derecho
 				eliminar(root.right, minNodeForRight.data);
- 
+
 			}
 			// verifica Si el nodo a ser borrado tiene solo un hijo izquierdo
 			else if (root.left != null) {
@@ -54,13 +44,13 @@ public class ArbolBusquedaBinaria {
 		}
 		return root;
 	}
- 
+
 	public static TreeNode Insertar(TreeNode root, TreeNode nodoaInsertar) {
 		if (root == null) {
 			root = nodoaInsertar;
 			return root;
 		}
- 
+
 		if (root.data > nodoaInsertar.data) {
 			if (root.left == null)
 				root.left = nodoaInsertar;
@@ -73,7 +63,7 @@ public class ArbolBusquedaBinaria {
 				Insertar(root.right, nodoaInsertar);
 		return root;
 	}
- 
+
 	public static void inOrder(TreeNode root) {
 		if (root == null)
 			return;
@@ -81,14 +71,16 @@ public class ArbolBusquedaBinaria {
 		System.out.print(root.data + " ");
 		inOrder(root.right);
 	}
+
 	public static void preOrder(TreeNode root) {
-		if( root != null) {
+		if (root != null) {
 			System.out.print(root.data + " ");
 			inOrder(root.left);
 			inOrder(root.right);
 		}
-		
+
 	}
+
 	public static void postOrder(TreeNode root) {
 		if (root != null) {
 			inOrder(root.left);
@@ -96,37 +88,13 @@ public class ArbolBusquedaBinaria {
 			System.out.print(root.data + " ");
 		}
 	}
- 
-	public static void main(String[] args) {
- 
-		// Acá se crea un árbol de búsqueda binaria
-		TreeNode rootNode = CrearArbolBinario();
- 
-		System.out.println("Árbol Binario en recorrido In-Orden");
-		inOrder(rootNode);
-		System.out.println();
-		int NodoaEliminar = 40;
-		TreeNode rootNodeRes = eliminar(rootNode, NodoaEliminar);
-		System.out.println("Árbol sin el elemento " + NodoaEliminar + " eliminado");
-		inOrder(rootNodeRes);
-		System.out.println();
-		System.out.println("Recorrido Pre-Orden" );
-		preOrder(rootNodeRes);
-		System.out.println();
-		System.out.println("Recorrido Post-Orden" );
-		postOrder(rootNodeRes);
-		
-		
 
-	}
- 
 	public static TreeNode CrearArbolBinario() {
 		TreeNode rootNode = new TreeNode(40);
 		TreeNode node20 = new TreeNode(20);
 		TreeNode node10 = new TreeNode(10);
 		TreeNode node30 = new TreeNode(30);
 
- 
 		Insertar(null, rootNode);
 		Insertar(rootNode, node20);
 		Insertar(rootNode, node10);
@@ -134,4 +102,23 @@ public class ArbolBusquedaBinaria {
 
 		return rootNode;
 	}
+
+	public static void main(String[] args) {
+		// Aca se crea un Arbol de busqueda binaria
+		TreeNode rootNode = CrearArbolBinario();
+		System.out.println("Arbol Binario en recorrido In-Orden");
+		inOrder(rootNode);
+		System.out.println();
+		int NodoaEliminar = 40;
+		TreeNode rootNodeRes = eliminar(rootNode, NodoaEliminar);
+		System.out.println("Arbol sin el elemento " + NodoaEliminar + " eliminado");
+		inOrder(rootNodeRes);
+		System.out.println();
+		System.out.println("Recorrido Pre-Orden");
+		preOrder(rootNodeRes);
+		System.out.println();
+		System.out.println("Recorrido Post-Orden");
+		postOrder(rootNodeRes);
+	}
+
 }
